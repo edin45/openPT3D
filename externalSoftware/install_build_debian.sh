580e0102d7216b53f4c01a95d5205717b3dad809
Edin Spiegel <apps4trainers@gmail.com>
2021-12-28 16:48:45 +0200

    Fixed install on Debian based Systems

--- externalSoftware/install_build_debian.sh
+++ externalSoftware/install_build_debian.sh
@@ -9,6 +9,34 @@ apt-get upgrade
 wait
 apt-get update -qq && apt-get install -qq
 wait
+apt install libopencv-calib3d-dev
+apt install libopencv-contrib-dev
+apt install libopencv-core-dev
+apt install libopencv-dnn-dev
+apt install libopencv-features2d-dev
+apt install libopencv-flann-dev
+apt install libopencv-highgui-dev
+apt install libopencv-imgcodecs-dev
+apt install libopencv-imgproc-dev
+apt install libopencv-ml-dev
+apt install libopencv-objdetect-dev
+apt install libopencv-photo-dev
+apt install libopencv-shape-dev
+apt install libopencv-stitching-dev
+apt install libopencv-superres-dev
+apt install libopencv-video-dev
+apt install libopencv-videoio-dev
+apt install libopencv-videostab-dev
+apt install libopencv-viz-dev
+apt install libopencv-calib3d4.5
+apt install libopencv-contrib4.5
+apt install libopencv-features2d4.5
+apt install libopencv-highgui4.5
+apt install libopencv-imgcodecs4.5
+apt install libopencv-imgproc4.5
+apt install libopencv-videoio4.5
+apt install libilmbase-dev
+wait
 wget -c 'http://ftp.de.debian.org/debian/pool/main/libp/libpng1.6/libpng-dev_1.6.37-3_amd64.deb'
 wget -c 'http://ftp.de.debian.org/debian/pool/main/libj/libjpeg-turbo/libjpeg-dev_2.0.6-4_amd64.deb'
 wget -c 'http://ftp.de.debian.org/debian/pool/main/t/tiff/libtiff-dev_4.2.0-1_amd64.deb'
