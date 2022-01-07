9da7faa234ac6420be17b50dfd19ddc3f3c424ba
Edin Spiegel <apps4trainers@gmail.com>
2022-01-07 15:15:56 +0200

    Bugfix test windows

--- externalSoftware/install_build_windows.bat
+++ externalSoftware/install_build_windows.bat
@@ -1,6 +1,6 @@
 git clone https://github.com/Microsoft/vcpkg
 cd vcpkg
-./bootstrap-vcpkg.bat
-vcpkg.exe install cereal ceres eigen3 libjpeg-turbo libpng tiff boost
+.\bootstrap-vcpkg.bat
+.\vcpkg.exe install cereal ceres eigen3 libjpeg-turbo libpng tiff boost
 cd ..
 VC_redist.x64.exe
