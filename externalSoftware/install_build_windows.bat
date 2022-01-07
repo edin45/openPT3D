8486a04838abec07a06167d2b87671baa15f180c
Edin Spiegel <apps4trainers@gmail.com>
2022-01-07 17:38:43 +0200

    Fixed Windows

--- externalSoftware/install_build_windows.bat
+++ externalSoftware/install_build_windows.bat
@@ -1,6 +1,6 @@
-git clone https://github.com/Microsoft/vcpkg
-cd vcpkg
-.\bootstrap-vcpkg.bat
-.\vcpkg.exe install cereal ceres eigen3 libjpeg-turbo libpng tiff boost
-cd ..
-VC_redist.x64.exe
+CALL git clone https://github.com/Microsoft/vcpkg
+CALL cd vcpkg
+CALL .\bootstrap-vcpkg.bat
+CALL .\vcpkg.exe install cereal ceres eigen3 libjpeg-turbo libpng tiff boost
+CALL cd ..
+CALL VC_redist.x64.exe
