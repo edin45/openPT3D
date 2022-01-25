bf2f7aaf524c36035b5865e0cf71ce4f341bb5a1
Edin Spiegel <edin@pop-os.localdomain>
2022-01-25 17:01:18 +0200

    possible fix for debian based systems

--- externalSoftware/install_build_debian.sh
+++ externalSoftware/install_build_debian.sh
@@ -28,4 +28,62 @@ wait
 wait
 ./b2 stage -j4 threading=multi link=shared
 wait
-./b2 install threading=multi link=shared           
+./b2 install threading=multi link=shared        
+#Prepare and empty machine for building:
+sudo apt-get update -qq && sudo apt-get install -qq
+wait
+sudo apt-get -y install git cmake libpng-dev libjpeg-dev libtiff-dev libglu1-mesa-dev
+wait
+main_path=`pwd`
+wait
+#Eigen (Required)
+git clone https://gitlab.com/libeigen/eigen.git --branch 3.4
+wait
+mkdir eigen_build && cd eigen_build
+wait
+cmake . ../eigen
+wait
+make && sudo make install
+wait
+cd ..
+wait
+#Boost (Required)
+sudo apt-get -y install libboost-iostreams-dev libboost-program-options-dev libboost-system-dev libboost-serialization-dev
+wait
+#OpenCV (Required)
+sudo apt-get -y install libopencv-dev
+wait
+#CGAL (Required)
+sudo apt-get -y install libcgal-dev libcgal-qt5-dev
+wait
+#VCGLib (Required)
+git clone https://github.com/cdcseacave/VCG.git vcglib
+wait
+#Ceres (Optional)
+sudo apt-get -y install libatlas-base-dev libsuitesparse-dev
+wait
+git clone https://ceres-solver.googlesource.com/ceres-solver ceres-solver
+wait
+mkdir ceres_build && cd ceres_build
+wait
+cmake . ../ceres-solver/ -DMINIGLOG=ON -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF
+wait
+make -j2 && sudo make install
+wait
+cd ..
+wait
+#GLFW3 (Optional)
+sudo apt-get -y install freeglut3-dev libglew-dev libglfw3-dev
+wait
+#OpenMVS
+git clone https://github.com/cdcseacave/openMVS.git openMVS
+wait
+mkdir openMVS_Linux_CPU && cd openMVS_Linux_CPU
+wait
+cmake . ../openMVS -DCMAKE_BUILD_TYPE=Release -DVCG_ROOT="$main_path/vcglib"
+
+#If you want to use OpenMVS as shared library, add to the CMake command:
+-DBUILD_SHARED_LIBS=ON
+
+#Install OpenMVS library (optional):
+make -j2 && sudo make installs   
