b4b734690e95ac20cf813f8f6e0eae577821b1a5
Edin Spiegel <apps4trainers@gmail.com>
2022-01-26 20:53:49 +0200

    install build arch bugfix test

--- externalSoftware/install_build_arch.sh
+++ externalSoftware/install_build_arch.sh
@@ -3,6 +3,20 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
+CPU_OR_GPU = 0
+for arg in "$@"
+do
+    case $arg in
+        -gpu|--gpu)
+        CPU_OR_GPU=1
+        shift
+        ;;
+        -cpu|--cpu)
+        CPU_OR_GPU=0
+        shift
+        ;;
+    esac
+done
 pacman -S opencv gperf
 wait
 pacman -S cgal
@@ -37,6 +51,8 @@ wait
 wait
 cd ..
 wait
+sudo pacman -S cmake libpng libjpeg libtiff mesa
+wait
 main_path=`pwd`
 wait
 #Eigen (Required)
@@ -49,6 +65,12 @@ wait
 make && sudo make install
 wait
 cd ..
+wait
+pacman -S libstdc++5
+wait
+pacman -Syu lib32-gcc-libs
+wait
+pacman -S gcc=libs
 wait
 #VCGLib (Required)
 git clone https://github.com/cdcseacave/VCG.git vcglib
@@ -63,12 +85,23 @@ make -j6 && sudo make install
 wait
 cd ..
 wait
+pacman -S freeglut glew glfw
+wait
 #OpenMVS
 git clone https://github.com/cdcseacave/openMVS.git openMVS
 wait
 mkdir openMVS_Linux_CPU && cd openMVS_Linux_CPU
 wait
-cmake . ../openMVS -DCMAKE_BUILD_TYPE=Release -DVCG_ROOT="$main_path/vcglib"
+if [ CPU_OR_GPU -ne 0 ]
+  then
+    echo "Using CUDA"
+    cmake . ../openMVS -DCMAKE_BUILD_TYPE=Release -DVCG_ROOT="$main_path/vcglib"
+fi
+if [ CPU_OR_GPU -ne 1 ]
+  then
+    echo "Using CPU" 
+    cmake . ../openMVS -DCMAKE_BUILD_TYPE=Release -DVCG_ROOT="$main_path/vcglib" -DOpenMVS_USE_CUDA=OFF
+fi
 wait
 #Install OpenMVS library (optional):
 make -j6
