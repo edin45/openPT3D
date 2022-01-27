ad42737b2bf4d7ae26e3bfb73a91251cdca69081
Edin Spiegel <apps4trainers@gmail.com>
2022-01-27 19:56:02 +0200

    Working on arch

--- externalSoftware/install_build_arch.sh
+++ externalSoftware/install_build_arch.sh
@@ -3,20 +3,6 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-CPU_OR_GPU = 0
-for arg in "$@"
-do
-    case $arg in
-        -gpu|--gpu)
-        CPU_OR_GPU=1
-        shift
-        ;;
-        -cpu|--cpu)
-        CPU_OR_GPU=0
-        shift
-        ;;
-    esac
-done
 pacman -S opencv gperf
 wait
 pacman -S cgal
@@ -27,31 +13,33 @@ pacman -S vtk
 wait
 pacman -S curl zip unzip tar
 wait
-git clone https://github.com/Microsoft/vcpkg
+#git clone https://github.com/Microsoft/vcpkg
+#wait
+#cd vcpkg
+#wait
+#./bootstrap-vcpkg.sh
+#wait
+#./vcpkg install cereal ceres eigen3 libjpeg-turbo libpng tiff
+#wait
+#cd ..
+#wait
+#wget -c "https://boostorg.jfrog.io/artifactory/main/release/1.74.0/source/boost_1_74_0.tar.bz2"
+#wait
+#tar -xf boost_1_74_0.tar.bz2
+#wait
+#cd boost_1_74_0
+#wait
+#./bootstrap.sh --prefix=/usr --with-python=python3 &&
+#wait
+#./b2 stage -j4 threading=multi link=shared
+#wait
+#./b2 install threading=multi link=shared           
+#wait
+#cd ..
+#wait
+pacman -S cmake libpng libjpeg libtiff mesa boost
 wait
-cd vcpkg
-wait
-./bootstrap-vcpkg.sh
-wait
-./vcpkg install cereal ceres eigen3 libjpeg-turbo libpng tiff
-wait
-cd ..
-wait
-wget -c "https://boostorg.jfrog.io/artifactory/main/release/1.74.0/source/boost_1_74_0.tar.bz2"
-wait
-tar -xf boost_1_74_0.tar.bz2
-wait
-cd boost_1_74_0
-wait
-./bootstrap.sh --prefix=/usr --with-python=python3 &&
-wait
-./b2 stage -j4 threading=multi link=shared
-wait
-./b2 install threading=multi link=shared           
-wait
-cd ..
-wait
-sudo pacman -S cmake libpng libjpeg libtiff mesa
+pacman -S graphviz
 wait
 main_path=`pwd`
 wait
@@ -71,6 +59,8 @@ wait
 pacman -Syu lib32-gcc-libs
 wait
 pacman -S gcc=libs
+wait
+pacman -S egl-wayland eglexternalplatform eigen gflags google-glog ninja
 wait
 #VCGLib (Required)
 git clone https://github.com/cdcseacave/VCG.git vcglib
@@ -92,16 +82,8 @@ git clone https://github.com/cdcseacave/openMVS.git openMVS
 wait
 mkdir openMVS_Linux_CPU && cd openMVS_Linux_CPU
 wait
-if [ CPU_OR_GPU -ne 0 ]
-  then
-    echo "Using CUDA"
-    cmake . ../openMVS -DCMAKE_BUILD_TYPE=Release -DVCG_ROOT="$main_path/vcglib"
-fi
-if [ CPU_OR_GPU -ne 1 ]
-  then
-    echo "Using CPU" 
-    cmake . ../openMVS -DCMAKE_BUILD_TYPE=Release -DVCG_ROOT="$main_path/vcglib" -DOpenMVS_USE_CUDA=OFF
-fi
+echo "Using CPU" 
+cmake . ../openMVS -DCMAKE_BUILD_TYPE=Release -DVCG_ROOT="$main_path/vcglib" -DOpenMVS_USE_CUDA=OFF
 wait
 #Install OpenMVS library (optional):
 make -j6
