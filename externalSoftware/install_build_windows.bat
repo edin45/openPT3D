7aa4ddc0663bcc67daabb1e4d39fa6d9ad20d6c9
Edin Spiegel <apps4trainers@gmail.com>
2022-04-01 16:19:56 +0200

    windows install speed up

--- externalSoftware/install_build_windows.bat
+++ externalSoftware/install_build_windows.bat
@@ -1,6 +1,1 @@
-CALL git clone https://github.com/Microsoft/vcpkg
-CALL cd vcpkg
-CALL .\bootstrap-vcpkg.bat
-CALL .\vcpkg.exe install cereal ceres eigen3 libjpeg-turbo libpng tiff boost
-CALL cd ..
 CALL VC_redist.x64.exe
