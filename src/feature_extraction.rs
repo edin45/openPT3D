cd40844e859c858fd557198694f7d056fecb956d
Edin Spiegel <apps4trainers@gmail.com>
2022-04-25 18:33:16 +0300

    continue work on feature extraction

--- src/feature_extraction.rs
+++ src/feature_extraction.rs
@@ -3,51 +3,93 @@ use image::GenericImageView;
 use std::path::Path;
 use std::collections::HashMap;
 
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
-
-pub fn extract_features(img_path: &str,feature_difference_threshold: i16) {
+pub fn extract_features(img_path: &str,feature_difference_threshold: i16,feature_size: u32) {
+    //Open the Image
     let img = image::open(&Path::new(img_path)).unwrap();
     let img_width = img.dimensions().0;
     let img_height = img.dimensions().1;
 
     println!("width: {}, height: {}",img_width,img_height);
 
-    //let mut contrasty_pixels = HashMap<Tuple,Tuple>::new();
-    let mut index = 0;
+    let mut index:i32 = 0;
     
-    let img_pixels = img.pixels();
-
     let mut previous_pixel_data:(u32, u32, image::Rgba<u8>) = (0,0,img.get_pixel(0,0));
 
     let mut imgbuf = image::ImageBuffer::new(img_width, img_height);
 
     let mut contrasty_pixels = HashMap::<String,bool>::new();
 
+    let mut occupied_pixels = HashMap::<String,bool>::new();
+//: Vec<Vec<image::Rgba<u8>>>
+    let mut final_features: Vec::<Vec::<image::Rgba<u8>>> = Vec::new();
+    let mut final_features_pixel_indexes: Vec::<Vec::<(u32,u32)>> = Vec::new();
+
+    //Iterate over the Pixels in the image
     for p in img.pixels() {
-        if index > 0 {
+
+        if index > 0 && index as i32 % img_width as i32 != 0 {
+            //Calc. the difference of the current pixel for each channel 
             let mut r_diff = p.2.0[0] as i16 - previous_pixel_data.2.0[0] as i16;
             let mut g_diff = p.2.0[1] as i16 - previous_pixel_data.2.0[1] as i16;
             let mut b_diff = p.2.0[2] as i16 - previous_pixel_data.2.0[2] as i16;
+
+            //If any of the differences are negative make them positive
             if r_diff < 0 {
                 r_diff = r_diff*-1;
             }
             if g_diff < 0 {
-                r_diff = r_diff*-1;
+                g_diff = g_diff*-1;
             }
             if b_diff < 0 {
-                r_diff = r_diff*-1;
+                b_diff = b_diff*-1;
             }
+
+            //Calc. the total_difference
             let total_difference = r_diff+g_diff+b_diff;
+
+            //If the total difference is greater than the feature difference threshold the current pixel will be added to the contrasty pixels HashMap
             if total_difference > feature_difference_threshold {
-                println!("pixels: {}, {} have a total difference of {}",p.0,p.1,total_difference);
                 contrasty_pixels.insert(format!("{}{}",p.0,p.1),true);
             }
         }
         previous_pixel_data = p;
         index+=1;
     }
+
+    let mut final_feature_index = 0;
+    let mut should_be_part_of_inflated_feature = 0;
+
+    for p in img.pixels() {
+        let key = format!("{}{}",p.0,p.1);
+        if contrasty_pixels.contains_key(&key) && p.0 + feature_size < img_width && p.1 + feature_size < img_height {
+            let mut final_features_pixel_indexes_temp: Vec<(u32,u32)> = vec![];
+            let mut are_any_pixels_occupied = false;
+            for x in 0..feature_size {
+                if(are_any_pixels_occupied) {
+                    break;
+                }
+                for y in 0..feature_size {
+                    let key = format!("{}{}",x+p.0,y+p.1);
+                    if(occupied_pixels.contains_key(&key)) {
+                        are_any_pixels_occupied = true;
+                        break;
+                    }
+                    final_features_pixel_indexes_temp.push((x+p.0,y+p.1));
+                    occupied_pixels.insert(format!("{}{}"),x+p.0,y+p.1,true);
+                }
+            }
+            if(!are_any_pixels_occupied) {
+                final_features_pixel_indexes.push(final_features_pixel_indexes_temp);
+            }
+        }
+        final_feature_index+=1;
+    }
+
+    for p in img.pixels() {
+
+    }
+
+    println!("Found {} features",contrasty_pixels.len());
 
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let key = format!("{}{}",x,y);
@@ -58,7 +100,11 @@ pub fn extract_features(img_path: &str,feature_difference_threshold: i16) {
         }
     }
 
+    println!("Saving image");
+
     imgbuf.save("result.png").unwrap();
+
+    println!("Saved image");
 
     // for p in img.pixels() {
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod feature_extraction;
 
 fn main() {
-    feature_extraction::extract_features("/media/edin/21e2cbef-5a87-475b-8753-31755e0a279d1/3dscanning/wood_watering_place_001/processed/IMG_6213.tif",30);
+    feature_extraction::extract_features("/media/edin/21e2cbef-5a87-475b-8753-31755e0a279d1/3dscanning/wood_watering_place_001/processed/IMG_6213.tif",35,50);
 }
